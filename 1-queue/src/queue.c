e78455ff0d87ac0b2c010ff6d46d006545f50567
Cesar Torres <cearto@berkeley.edu>
2024-04-11 18:00:57 +0000

    4/11 end of implementation

--- 1-queue/src/queue.c
+++ 1-queue/src/queue.c
@@ -1,5 +1,12 @@
 #include "queue.h"
 
+
+
+void queue_reset(Queue* queue){
+    q->head = q->data;
+    q->tail = q->data;
+}
+
 Queue* queue_create(int capacity){
     assert(capacity > 0);
     Queue* q = (Queue*) malloc(sizeof(Queue));
@@ -7,8 +14,7 @@ Queue* queue_create(int capacity){
 
     // error handling malloc
 
-    q->head = q->data;
-    q->tail = q->data;
+    queue_reset(q);
     q->capacity = capacity;  
     return q; 
 }
@@ -29,6 +35,28 @@ void queue_pprint(Queue* queue){
     }
 }
 
+
 bool queue_enqueue(Queue* queue, int value){
-    
+    // check queue size -- if full, return false; fprintf --> Overflow
+    *(queue->tail) = value; //*(0x1213930909) = value
+    queue->tail++;
+    return true;
+}
+
+int queue_dequeue(Queue* queue){
+    // check queue size -- if empty, exit underflow message
+    int data = *(queue->head); // *(0x1283919);
+    *(queue->head) = -1;
+    queue->head++;
+    if(queue->head == queue->tail){
+        queue_reset(queue);
+    }
+    return -1;
+}
+
+bool queue_is_empty(Queue* queue){
+    return queue->head == queue->tail;
+}
+bool queue_is_full(Queue* queue){
+    return (queue->tail - queue->head) == queue->capacity;
 }
